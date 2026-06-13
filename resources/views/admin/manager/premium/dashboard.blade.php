f504a88051d063839b4cff4bcf90e56a5c843069
HassanKindamba <kindambahassani2005@gmail.com>
2026-06-13 21:04:25 +0300

    Add premium dashboard page

---
+++ resources/views/admin/manager/premium/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.premium')
+
+@section('content')
+
+<div class="row">
+
+    <div class="card">
+        <h3 class="gold">Total Premium Users</h3>
+        <p>0</p>
+    </div>
+
+    <div class="card">
+        <h3 class="gold">Active Subscriptions</h3>
+        <p>0</p>
+    </div>
+
+    <div class="card">
+        <h3 class="gold">Revenue</h3>
+        <p>TZS 0</p>
+    </div>
+
+</div>
+
+@endsection
