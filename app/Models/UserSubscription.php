e07bd94385299f73494b3b34a5d5d3c98e0a1f3c
HassanKindamba <kindambahassani2005@gmail.com>
2026-06-14 16:15:24 +0300

    Add UserSubscription model

---
+++ app/Models/UserSubscription.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserSubscription extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'subscription_plan_id',
+        'starts_at',
+        'expires_at',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(SubscriptionPlan::class, 'subscription_plan_id');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
