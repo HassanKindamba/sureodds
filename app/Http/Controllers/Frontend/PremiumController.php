66a68c9f76a7ebb1064349bdd8bed0761267c58a
HassanKindamba <kindambahassani2005@gmail.com>
2026-06-14 16:15:23 +0300

    Update frontend PremiumController

--- app/Http/Controllers/Frontend/PremiumController.php
+++ app/Http/Controllers/Frontend/PremiumController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\SubscriptionPlan;
 
 class PremiumController extends Controller
 {
-    public function premium(){
-        return view('frontend.premium');
+    public function premium()
+    {
+        $plans = SubscriptionPlan::where('status', true)->get();
+
+        return view('frontend.premium', compact('plans'));
     }
 }
