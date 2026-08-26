e8ba47a18c5f2cf8a7f90c4446f0fefb8cc5b741
HassanKindamba <kindambahassani2005@gmail.com>
2026-08-26 18:58:24 +0300

    Add ChatMessage model

---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'message',
+        'image_path',
+        'is_pinned',
+        'pinned_at',
+    ];
+
+    protected $casts = [
+        'is_pinned' => 'boolean',
+        'pinned_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
