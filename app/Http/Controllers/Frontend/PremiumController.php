a9c0edcf8fc613972d7bd91e63a3dee7afba7382
HassanKindamba <kindambahassani2005@gmail.com>
2026-08-27 07:17:34 +0300

    Update PremiumController

--- app/Http/Controllers/Frontend/PremiumController.php
+++ app/Http/Controllers/Frontend/PremiumController.php
@@ -10,7 +10,10 @@ class PremiumController extends Controller
 {
     public function premium()
     {
-        $plans = SubscriptionPlan::where('status', true)->get();
+        // 1. Inachukua mipango yote yenye status true na kuipanga kuanzia bei ndogo (Wiki) kwenda kubwa (Mwezi)
+        $plans = SubscriptionPlan::where('status', true)
+                                  ->orderBy('price', 'asc')
+                                  ->get();
 
         return view('frontend.premium', compact('plans'));
     }
