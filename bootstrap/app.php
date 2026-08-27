30731d791018ce9fe402c539232754170262f8ce
HassanKindamba <kindambahassani2005@gmail.com>
2026-08-27 07:17:41 +0300

    Update bootstrap app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,14 +8,16 @@ use Illuminate\Console\Scheduling\Schedule;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
 
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-        'premium' => \App\Http\Middleware\CheckPremium::class,
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'premium' => \App\Http\Middleware\CheckPremium::class,
+            'vip.access' => \App\Http\Middleware\EnsureIsPremiumUser::class,
         ]);
     })
 
